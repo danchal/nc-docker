4b5268b2e8686557cc5e9efb3cb96bd91e2b42f0
danchal <danchal@users.noreply.github.com>
2019-02-27 17:46:56 +0100

    working

--- .examples/docker-compose/with-nginx-proxy/mariadb-cron-redis/fpm/backup/backup-daily.sh
+++ .examples/docker-compose/with-nginx-proxy/mariadb-cron-redis/fpm/backup/backup-daily.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # code goes here.
-set -ex
+set -e
 
 export THE_DATE=$(date +%Y-%m-%d-%s)
-echo "Starting Backup on ${THE_DATE}"
+echo "Starting $0 on ${THE_DATE}"
 
 restic-runner --repo gdrive --set nextcloud backup
--- .examples/docker-compose/with-nginx-proxy/mariadb-cron-redis/fpm/backup/entrypoint.sh
+++ .examples/docker-compose/with-nginx-proxy/mariadb-cron-redis/fpm/backup/entrypoint.sh
@@ -3,12 +3,5 @@ set -e
 
 echo "Starting container ..."
 
-# initialise restic repositories - TODO
-# 
-# if [ ! -f "$RESTIC_REPOSITORY/config" ]; then
-#     echo "Restic repository '${RESTIC_REPOSITORY}' does not exists. Running restic init."
-#     restic init | true
-# fi
-
 # start cron
 /usr/sbin/crond -f -l 8

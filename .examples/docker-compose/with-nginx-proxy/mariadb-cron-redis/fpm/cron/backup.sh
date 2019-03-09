926cd458ce2fe42bed6ee5f3a0998cacee688d9b
danchal <danchal@users.noreply.github.com>
2019-03-09 23:02:55 +0100

    wip

--- .examples/docker-compose/with-nginx-proxy/mariadb-cron-redis/fpm/cron/backup.sh
+++ .examples/docker-compose/with-nginx-proxy/mariadb-cron-redis/fpm/cron/backup.sh
@@ -1,18 +1,41 @@
 #!/bin/sh
-
 THE_DATE=$(date '+%a %d-%b-%Y %T %Z')
-error=1
+error=
+trap finish EXIT
+command=$1
 
-report=$(borgmatic -c ${BORG_CONFIG_DIR} 2>&1)
-result=$?
+finish(){
+    set_maintenance_mode off
+    exit $error
+}
 
-if [ $result -eq 0 ]; then
-    echo "ok"
-    error=0
-else
-    echo "FAILED - $report"
-    source /app/signal.sh "FAILED - ${0} - $(hostname) - ${THE_DATE} ${report}"                
-    echo "report sent"
+set_maintenance_mode(){
+    su www-data -s /bin/sh -c "php occ maintenance:mode --$1"
+}
+
+do_borgmatic(){
+    report=$(borgmatic -c ${BORG_CONFIG_DIR} --$1 2>&1)
+    ret_val=$?
+
+    if [ $ret_val -ne 0 ]; then
+        source /app/signal.sh "FAILED - ${1} - $(hostname): ${0} - ${THE_DATE} ${report}"
+        error=1
+    fi
+}
+
+do_borgmatic prune
+
+set_maintenance_mode on
+
+do_borgmatic create
+
+# backup DB
+mysqldump --single-transaction -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} > nextcloud-sqlbkp_`date +"%Y%m%d%H%M%S"`.bak
+
+set_maintenance_mode off
+
+# rclone
+
+if [ $command ] && [ $command == "check" ]; then
+    do_borgmatic check
 fi
-
-exit $error

587e2d34ff53ff2d4694a4ea31cd6ccdf9b04ed0
danchal <danchal@users.noreply.github.com>
2019-03-22 14:53:03 +0100

    shellcheck cleaned

--- .examples/docker-compose/with-nginx-proxy/mariadb-cron-redis/fpm/cron/app/backup.sh
+++ .examples/docker-compose/with-nginx-proxy/mariadb-cron-redis/fpm/cron/app/backup.sh
@@ -14,23 +14,23 @@ backuplog=$(mktemp)
     do_exit(){
         LASTERR="$1"
         message="PASS - $(hostname): ${THE_DATE} - Backup"
-        
-        if [ $LASTERR -ne 0 ]; then
+
+        if [ "$LASTERR" -ne 0 ]; then
             message="FAILED - $(hostname): ${THE_DATE} - [${LASTERR}] Backup"
         fi
-        
+
         echo "$message"
-        source /app/signal.sh "$(cat $backuplog)"
-        rm $backuplog
-        exit $LASTERR
+        /app/signal.sh "$(cat "${backuplog}")"
+        rm "$backuplog"
+        exit "$LASTERR"
     }
 
     handle_return(){
-        ret_val=$1
-        err_code=$2
+        ret_val="$1"
+        err_code="$2"
 
-        if [ $ret_val -ne 0 ]; then
-            error=$err_code
+        if [ "$ret_val" -ne 0 ]; then
+            error="$err_code"
         fi
     }
 
@@ -38,12 +38,12 @@ backuplog=$(mktemp)
         echo "Do: validation"
 
         # error if not set
-        if [ -z $RCLONE_REMOTE ]; then
+        if [ -z "$RCLONE_REMOTE" ]; then
             echo "Environment variable RCLONE_REMOTE not set"
             error=101
         fi
 
-        if [ -z $NEXTCLOUD_INSTANCEID ]; then
+        if [ -z "$NEXTCLOUD_INSTANCEID" ]; then
             echo "${report} - Environment variable NEXTCLOUD_INSTANCEID not set"
             error=102
         fi
@@ -58,11 +58,11 @@ backuplog=$(mktemp)
 
         case ${command} in
             borgmatic)
-                borgmatic -c ${BORG_CONFIG_DIR} --${p1}
+                borgmatic -c "${BORG_CONFIG_DIR}" "--${p1}"
                 handle_return $? 111
                 ;;
             rclone)
-                rclone ${p1} ${p2} ${RCLONE_REMOTE}:${NEXTCLOUD_INSTANCEID} ${p3}
+                rclone "${p1}" "${p2}" "${RCLONE_REMOTE}:${NEXTCLOUD_INSTANCEID}" ${p3:+"$p3"}
                 handle_return $? 112
                 ;;
             maintenancemode)
@@ -72,15 +72,15 @@ backuplog=$(mktemp)
             dbdump)
                 mysqldump \
                     --single-transaction \
-                    --result_file=/dbdump/nextcloud-${NEXTCLOUD_INSTANCEID}_$(date +"%Y%m%d%H%M%S").bak \
-                    -h ${MYSQL_HOST} \
-                    -u ${MYSQL_USER} \
-                    -p${MYSQL_PASSWORD} \
-                    ${MYSQL_DATABASE}
+                    --result_file="/dbdump/nextcloud-${NEXTCLOUD_INSTANCEID}_$(date +"%Y%m%d%H%M%S").bak" \
+                    -h "${MYSQL_HOST}" \
+                    -u "${MYSQL_USER}" \
+                    -p"${MYSQL_PASSWORD}" \
+                    "${MYSQL_DATABASE}"
                 handle_return $? 114
                 ;;
             borgcheck)
-                borg --info check --verify-data ${THE_REPOSITORY}/*
+                borg --info check --verify-data "${THE_REPOSITORY}/*"
                 handle_return $? 115
                 ;;
             *)
@@ -97,13 +97,13 @@ backuplog=$(mktemp)
     do_command dbdump
     do_command borgmatic create
     do_command maintenancemode off
-    do_command rclone sync $THE_REPOSITORY
+    do_command rclone sync "${THE_REPOSITORY}"
 
-    if [ $THE_COMMAND ] && [ $THE_COMMAND == "check" ]; then
-        do_command rclone check $THE_REPOSITORY # must be before borg check (see below)
-        do_command borgmatic check              # borg check changes nonce file in repo?
+    if [ "$THE_COMMAND" ] && [ "$THE_COMMAND" = "check" ]; then
+        do_command rclone check "$THE_REPOSITORY" # must be before borg check (see below)
+        do_command borgmatic check                # borg check changes nonce file in repo?
         #do_command borgcheck
     fi
 
     exit $error
-} 2>&1 | tee -a ${backuplog}
+} 2>&1 | tee -a "${backuplog}"
